3c958d53e23be18ba8b54476571748a7b623af6c
jonathan1987 <jayhernandez1987@gmail.com>
2019-05-19 20:50:47 -0400

    Final revision of Project 2, made changes and addition to enet script

--- Project2/Project2_Group2_elasticnet.R
+++ Project2/Project2_Group2_elasticnet.R
@@ -22,6 +22,13 @@ PH_enet_model <- train(PH ~.,
                        tuneLength = 25,
                        trControl = train_control)
 
+# coefficients of best model based on min(RMSE)
+coefs <-coef(PH_enet_model$finalModel, PH_enet_model$bestTune$lambda)
+
+# zero coefficients, removed
+kable(coefs[,1][coefs[,1]==0])
+
+
 # extract best RMSE value along with alpha, lambda
 PH_enet_model$results[which.min(PH_enet_model$results[, "RMSE"]), ] %>%
   kable() %>% kable_styling(fixed_thead = T)
@@ -30,6 +37,21 @@ PH_enet_model$results[which.min(PH_enet_model$results[, "RMSE"]), ] %>%
 enet_pred_PH <- predict(PH_enet_model, newdata = train)
 rmse_train_enet <- postResample(enet_pred_PH, train$PH)
 
+# plot of actual PH vs predicted PH for the training dataset
+ggplot(data = train, aes(x=train$PH, y=enet_pred_PH)) +
+  geom_point() +
+  geom_smooth(method = "glm") +
+  xlab("Observed PH") +
+  ylab("Predicted PH")
+
+qplot(x=enet_pred_PH, y=residuals(PH_enet_model),
+      xlab = "Predicted PH", ylab = "Residuals") +
+  geom_hline(yintercept = 0)
+
 # make predictions on the test dataset on PH
 enet_pred_test_PH <- round(predict(PH_enet_model, newdata = test), 2)
 write.csv(enet_pred_test_PH, "prediction-PH-enet.csv", col.names = "PH")
+
+plot(varImp(PH_enet_model, scale = FALSE))
+
+PH_enet_model$results
